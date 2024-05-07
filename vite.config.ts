5d04dba55f0ee3f74b1c2d9300336199c14d2622
tinkuo6o71999 <20bcs099@nith.ac.in>
2024-05-07 23:20:39 +0530

    Logo added

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: 'public/index.html', // Adjust this to your entry file
-    }
-  },
 })
